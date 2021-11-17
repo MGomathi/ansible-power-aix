c5ddbe712b1c156966871dc309d3394f6169c94e
Gomathi Mohan <gomathimgit@gmail.com>
2021-11-17 11:15:21 -0500

    python3 compatibility fixes for nim_vios_hc.

--- plugins/modules/nim_vios_hc.py
+++ plugins/modules/nim_vios_hc.py
@@ -81,6 +81,7 @@ status:
 '''
 
 import re
+import os
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -368,9 +369,8 @@ def vios_health(module, mgmt_sys_uuid, hmc_ip, vios_uuids):
             1 otherwise
     """
     module.debug('hmc_ip: {0} vios_uuids: {1}'.format(hmc_ip, vios_uuids))
-
     # Build the vioshc cmd
-    cmd = [vioshc_cmd, '-i', hmc_ip, '-m', mgmt_sys_uuid]
+    cmd = [vioshc_interpreter, vioshc_cmd, '-i', hmc_ip, '-m', mgmt_sys_uuid]
     for uuid in vios_uuids:
         cmd.extend(['-U', uuid])
     if module._verbosity > 0:
@@ -413,7 +413,7 @@ def vios_health_init(module, hmc_id, hmc_ip):
     module.debug('hmc_id: {0}, hmc_ip: {1}'.format(hmc_id, hmc_ip))
 
     # Call the vioshc.py script a first time to collect UUIDs
-    cmd = [vioshc_cmd, '-i', hmc_ip, '-l', 'a']
+    cmd = [vioshc_interpreter, vioshc_cmd, '-i', hmc_ip, '-l', 'a']
     if module._verbosity > 0:
         cmd.extend(['-' + 'v' * module._verbosity])
         if module._verbosity >= 3:
@@ -596,11 +596,60 @@ def health_check(module, targets):
     return health_tab
 
 
+def get_vioshc_interpreter(module):
+    """
+    Provide the right python interpreter to be used for vioshc command.
+    
+    return: Right python interpreter if ok,
+            Null string otherwise
+    """
+    #Check whether the python interpreter is in the list.
+    #if that path exists on the system , then continue with that
+    #else proceed to the next interpreter in the list.
+    is_interpreter_found = False
+    is_pycurl_found= False
+    python_interpreter_list  = ['/usr/bin/python', '/opt/freeware/bin/python3', '/usr/bin/python3']
+    
+    for interpreter in python_interpreter_list:
+        #Reinitialize these values for every loop
+        is_interpreter_found = False
+        is_pycurl_found= False
+        #check if that path exists. Add check for pycurl module.
+        if os.path.isfile(interpreter):
+            is_interpreter_found = True
+            cmd = interpreter + '  -c "import pycurl"'
+            ret, stdout, stderr = module.run_command(cmd)
+            if ret == 0:
+                return interpreter
+            else:
+                is_pycurl_found = False
+        
+        
+    #If we have exhausted the list, that means no interpreter or no pycurl module
+    # in the list was found. So give a error message.The interpreter will be found
+    #always because without the ansible module will not run anyway.
+    if  is_interpreter_found == True:
+        if is_pycurl_found == False:
+            msg = 'Unable to find the right python interpreter with the dependent module pycurl.'
+            OUTPUT.append('    Warning: Dependent module pycurl is not found  ')
+            results['stdout'] = stdout
+            results['stderr'] = stderr
+    else:
+        msg = 'Unable to find the python interpreter for vioshc command. '
+        OUTPUT.append('    Warning: No python interpreter found. ')
+
+    results['msg'] = msg
+    return None;
+	
+	
+
+
 def main():
     global results
     global OUTPUT
     global NIM_NODE
     global vioshc_cmd
+    global vioshc_interpreter
 
     module = AnsibleModule(
         argument_spec=dict(
@@ -636,9 +685,16 @@ def main():
         OUTPUT.append('    Targets list: {0}'.format(target_list))
         module.debug('Targets list: {0}'.format(target_list))
 
-        # Check vioshc script is present, fail_json if not
+        # Get the interpreter for vioshc command.  If unable to find a
+        # proper interpreter, then fail.
+        vioshc_interpreter = get_vioshc_interpreter(module)
+        if(vioshc_interpreter is None):
+            module.fail_json(**results)
+                   
+        
+        # Get the vioshc command path
         vioshc_cmd = module.get_bin_path('vioshc.py', required=True)
-        module.debug('Using vioshc.py script at {0}'.format(vioshc_cmd))
+        module.debug('Using vioshc.py script at {0}:{1}'.format(vioshc_interpreter, vioshc_cmd))
 
         targets_health_status = health_check(module, target_list)
 
